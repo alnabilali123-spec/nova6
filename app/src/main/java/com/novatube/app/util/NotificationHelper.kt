6eb178e65d1f3446ee0cef0f34bfe150608a1954
alnabilali123-spec <alnabilali123@gmail.com>
2026-09-03 21:56:52 +0300

    Delete app directory

--- app/src/main/java/com/novatube/app/util/NotificationHelper.kt
+++
@@ -1,8 +0,0 @@
-package com.novatube.app.util
-
-object NotificationHelper {
-    const val CHANNEL_DOWNLOADS = "novatube_downloads"
-    const val CHANNEL_PLAYER = "novatube_player"
-    const val CHANNEL_MUSIC = "novatube_music"
-    const val CHANNEL_GENERAL = "novatube_general"
-}
